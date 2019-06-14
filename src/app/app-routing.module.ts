dba6d0bc995cbf26121368b1b311a00086895f30
Todd Aden <FluentZap@Gmail.com>
2019-06-14 14:45:45 -0700

    update routes, add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+    data: {preload: true}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// declare var variableName: any;
+// import * as variable from 'bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CharactersComponent } from './characters/characters.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CharactersComponent,
+    WelcomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
